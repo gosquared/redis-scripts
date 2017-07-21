0c3019e5d96b015d2cc901720d27d9169a888694
Geoff Wagstaff <geoff@gosquared.com>
2017-07-21 11:39:53 +0100

    add hmsetx

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # redis-scripts
-Redis scripts created while building GoSquared's customer analytics and chat platform
+GoSquared is customer analytics and chat platform. We've worked with Redis a lot and have developed scripts for it along the way.
+
+    npm install --save redis @gosquared/redis-scripts
+
+```javascript
+let createScripts = require('@gosquared/redis-scripts')
+let redis = require('redis');
+let redisScripts = createScripts(redis);
+
+// hmsetx
+let { hmsetx } = redisScripts;
+let key = 'your-key';
+let fields = { some_field: 'test', another_field: 'foo' };
+hmsetx(key, fields).then(handleResult).catch(handleError); 
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+const _ = require('lodash');
+let Scripty = require('node-redis-scripty');
+
+const hmsetx = `
+  local key = KEYS[1]
+  local args = ARGV
+  local call = redis.call
+
+  if call("EXISTS", key) == 0 then
+    return nil
+  end
+
+  return call("HMSET", key, unpack(args))
+`;
+
+module.exports = function(redis) {
+  let scripty = new Scripty(redis);
+
+  function hmsetx(key, fields) {
+    return new Promise((resolve, reject) => {
+      scripty.loadScript('hmsetx', hmsetx, (err, script) => {
+        if (err) return reject(err);
+
+        // Make sure any null values are set to '' for redis
+        Object.keys(fields).forEach(k => {
+          if (fields[k] === null) fields[k] = '';
+        });
+
+        let args = _.flatten(_.toPairs(fields));
+        let numKeys = 1;
+        script.run(numKeys, key, ...args, (err, result) => err ? reject(err) : resolve(result));
+      });
+    });
+  }
+
+  return { hmsetx };
+};
